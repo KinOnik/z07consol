32cc6fadcc8ccb7e25c1e6a98b80c89c30a99890
KinOnik <84587417+KinOnik@users.noreply.github.com>
2021-12-13 15:21:05 +0300

    Add files via upload

---
+++ 1consol/1consol/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+
+namespace _1consol
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("\n\tВведите строку: ");
+			StringBuilder a = new StringBuilder(Console.ReadLine());
+			Console.Write("\n\tКакой символ удалить: ");
+			char buf = Console.ReadKey().KeyChar;
+
+			a.Replace(buf.ToString().ToLower(), "");
+			a.Replace(buf.ToString().ToUpper(), "");
+			Console.Write($"\n\nИзмененная строка: {a}\n\n");
+		}
+	}
+}
---
+++ 2consol/2consol/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _2consol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.Write("\n\tВведите строку: ");
+            StringBuilder a = new StringBuilder(Console.ReadLine());
+            Console.Write($"\n\t Искомое слово: ");
+            string buffer = Console.ReadLine();
+
+            string[] buf = a.ToString().Split(' ', ',', '.', '!', '?', '-', '\'', '\\', '\"', '*');
+            for (int i = 0; i < buf.Length; i++)
+            {
+                if (buf[i].ToLower() == buffer.ToLower())
+                    sum++;
+            }
+            Console.Write($"\n\nКоличество повторений: {sum}\n\n");
+        }
+    }
+}
